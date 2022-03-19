0e8d308b322e40bbafe67fc51bb4cc2bc9c026f5
Pustelnyk David <forrestgamp1994@gmail.com>
2022-03-20 00:13:51 +0300

    new

--- Homework#3(Строки).py
+++ Homework#3(Строки).py
@@ -2,8 +2,7 @@ c = "замена в строке"
 b = input("Введите что-нибудь:")
 a = "Это строка в которую {} новую строку"
 print(a.format(b))
-a = a.format(c)
-print(a)
+print(a.format(c))
 print(len(a))
 if "строка" in a:
     print("Yes")
