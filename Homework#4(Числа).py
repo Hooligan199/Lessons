633991774ccfaf515f43e753952936cd6e4d58c4
Pustelnyk David <forrestgamp1994@gmail.com>
2022-03-20 19:11:44 +0300

    new

--- Homework#4(Числа).py
+++ Homework#4(Числа).py
@@ -1,9 +1,15 @@
 import sys
 
-a = int(input("Число:"))
+first_number = int(input("Число:"))
+revers_number = 0
 
-if 99 >= a <= 999 or a == -a:
+if 99 >= first_number <= 999 or first_number == -first_number:
     print("Только трехзначные положительные числа")
     sys.exit()
 
-print(str(a)[::-1])
+while first_number != 0:
+    digit = first_number % 10
+    revers_number = revers_number * 10 + digit
+    first_number //= 10
+
+print(revers_number)
