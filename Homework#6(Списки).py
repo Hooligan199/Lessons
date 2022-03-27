ddaac252d1ad66e0851b3b9fe740fd31f0023129
Pustelnyk David <forrestgamp1994@gmail.com>
2022-03-27 22:25:33 +0300

    Homework#6(Списки) (New)

--- Homework#6(Списки).py
+++ Homework#6(Списки).py
@@ -1,5 +1,9 @@
-g = [1,2,3,4,5,6,4,8,9,52]
+g = [1,2,3,4,5,6,4,8,9,52,2,201]
 
-h = [5,7,52,10,11,2,3]
+h = [5,7,52,10,11,2,3,100,82,7456,100000,159852]
 
-print([x for x in g if x in h])
+x = [x for x in g if x not in h]
+x.extend([x for x in h if x not in g])
+
+print(x)
+
