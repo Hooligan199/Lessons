2a3467e9064da7a0c26bbffb78f7abadacca4a8e
Pustelnyk David <forrestgamp1994@gmail.com>
2022-03-31 23:35:08 +0300

    Homework#9(Множества)

---
+++ Homework#9(Множества).py
@@ -0,0 +1,15 @@
+set_1 = {1, 2, 3, 4}
+
+set_2 = {2, 3, 4, 5}
+
+print(set_2, set_1)
+
+if set_2 - set_1 != set():
+    print("set_2 не входит в set_1")
+else:
+    print("set_2 входит в set_1")
+
+
+
+
+# print("\u0030")
