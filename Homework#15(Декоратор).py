5cec57883cf79043c494ec2f182f01aeffecdd03
Pustelnyk David <forrestgamp1994@gmail.com>
2022-04-10 19:58:02 +0300

    new

--- Homework#15(Декоратор).py
+++ Homework#15(Декоратор).py
@@ -1,23 +1,11 @@
 global_dict = {'user': "12345", 'qwerty': "pass", 'aBcD': "qwerty123"}
 
 
-def login(func):
-    def wrapper(log: str, password: str):
-
-
-
-
-
-@login
-def authenticate() -> bool:
-    return True
-
-
-@login
 def check_password(log: str, password: str) -> bool:
-    if global_dict.get(log, None) == password:
-        return True
+    if log in global_dict.keys():
+        return global_dict.get(account_login) == password
     return False
+
 
 if __name__ == '__main__':
     for i in range(4):
@@ -25,6 +13,15 @@ if __name__ == '__main__':
 
         account_password = str(input("Password:"))
 
+        if check_password(account_login, account_password) is True:
+            print("Вы в системе!")
+            break
 
+        elif i < 3:
+            print("Не правильное Имя или Пароль" 
+                  "\n" f"У вас осталось {3-i} попыток")
+
+        if i == 3:
+            print("Попытки истекли!")
 
 
